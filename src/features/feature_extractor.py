5189bafae5265b77d4b701e8f240823e1f3e7e96
tomstark99 <jb18789@bristol.ac.uk>
2021-04-30 22:28:52 +0100

    started converting to class files

---
+++ src/features/feature_extractor.py
@@ -0,0 +1,64 @@
+from typing import Dict, Any
+from tdqm import tdqm
+
+import numpy as np
+import torch as t
+import torch.nn as nn
+
+class FeatureExtractor:
+    """
+    Extracts image features from a 2D CNN backbone for every frame in all videos.
+    """
+    
+    def __init__(
+        self, 
+        backbone_2d: nn.Module, 
+        device: t.device, 
+        dtype: t.float, 
+        frame_batch_size: int = 128
+    ):
+        self.model = backbone_2d
+        self.device = device
+        self.dtype = dtype
+        self.frame_batch_size = frame_batch_size
+    
+    def extract(self, dataset: GulpDirectory, feature_writer: FeatureWriter) -> int:
+        total_instances = 0
+        self.model.eval()
+        for i, c in enumerate(tqdm(dataset)):
+            if i == 0:
+                for batch_input, batch_labels in c:
+        
+                    batch_input = np.array(batch_input).transpose(0,3,1,2)
+                    batch_input = t.tensor(batch_input, device=self.device, dtype=self.dtype)
+                    batch_input = batch_input.unsqueeze(0)
+
+                    batch_size, n_frames = batch_input.shape[:2]
+                    flattened_batch_input = batch_input.view((-1, *batch_input.shape[2:]))
+
+                    n_chunks = int(np.ceil(len(flattened_batch_input)/128))
+                    chunks = t.chunk(flattened_batch_input, n_chunks, dim=0)
+                    flatten_batch_features = []
+                    for chunk in chunks:
+                        chunk = chunk.unsqueeze(0)
+                        with t.no_grad():
+                            chunk_features = self.model.features(chunk.to(self.device))
+                            chunk_features = self.model.new_fc(chunk_features)
+                            flatten_batch_features.append(chunk_features.squeeze(0))
+                    flatten_batch_features = t.cat(flatten_batch_features, dim=0)
+                    batch_features = flatten_batch_features.view((batch_size, 
+                                                                  n_frames, 
+                                                                  *flatten_batch_features.shape[1:]))
+
+                    total_instances += batch_size
+                    self._append(batch_features, batch_labels, batch_size, feature_writer)
+        return total_instances
+
+    def _append(self, batch_features, batch_labels, batch_size, feature_writer):
+        batch_narration_id = batch_labels['narration_id']
+        batch_features = batch_features.cpu().numpy()
+        assert len(batch_narration_id) == batch_size
+        assert len(batch_labels) == batch_size
+        assert len(batch_features) == batch_size
+
+        feature_writer.append(batch_narration_id, batch_features, batch_labels)
---
+++ src/features/pkl.py
@@ -0,0 +1,29 @@
+from typing import Dict, Any
+from .feature_store import FeatureWriter
+from pathlib import Path
+
+import pickle
+import numpy as np
+
+class PickleFeatureWriter(FeatureWriter):
+    
+    def __init__(self, pkl_path: Path, features_dim: int):
+        self.pkl_path = pkl_path
+        self.features_dim = features_dim
+        self.narration_ids = []
+        self.features = []
+        self.labels = []
+        
+    def append(self, narration_id: str, featues: np.ndarray, labels: Dict[str, Any]) -> None:
+        assert features.shape[1] == self.features_dim
+        self.narration_ids.append(narration_id)
+        self.features.append(features)
+        self.labels.append(labels)
+        
+    def save(self):
+        with open(self.pkl_path, 'wb') as f:
+            pickle.dump({
+                'narration_id': self.narration_ids,
+                'features': np.concatenate(self.features),
+                'labels': self.labels
+            }, f)

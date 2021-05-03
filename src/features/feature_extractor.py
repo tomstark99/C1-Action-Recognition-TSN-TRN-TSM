2ccf5a34fe0d6a56fa1cd5f8609dcbef20578df6
tomstark99 <jb18789@bristol.ac.uk>
2021-05-03 11:43:15 +0100

    feature_extractor

--- src/features/feature_extractor.py
+++ src/features/feature_extractor.py
@@ -34,37 +34,36 @@ class FeatureExtractor:
             total=dataset.num_chunks,
             dynamic_ncols=True
         ):
-            if i == 0:
-                for i, (batch_input, batch_labels) in tqdm(
-                    enumerate(c),
-                    unit=" video",
-                    total=len(c.meta_dict),
-                    dynamic_ncols=True
-                ):
-        
-                    batch_input = np.array(batch_input).transpose(0,3,1,2)
-                    batch_input = t.tensor(batch_input, device=self.device, dtype=self.dtype)
-                    batch_input = batch_input.unsqueeze(0)
+            for j, (batch_input, batch_labels) in tqdm(
+                enumerate(c),
+                unit=" video",
+                total=len(c.meta_dict),
+                dynamic_ncols=True
+            ):
+            
+                batch_input = np.array(batch_input).transpose(0,3,1,2)
+                batch_input = t.tensor(batch_input, device=self.device, dtype=self.dtype)
+                batch_input = batch_input.unsqueeze(0)
 
-                    batch_size, n_frames = batch_input.shape[:2]
-                    flattened_batch_input = batch_input.view((-1, *batch_input.shape[2:]))
+                batch_size, n_frames = batch_input.shape[:2]
+                flattened_batch_input = batch_input.view((-1, *batch_input.shape[2:]))
 
-                    n_chunks = int(np.ceil(len(flattened_batch_input)/128))
-                    chunks = t.chunk(flattened_batch_input, n_chunks, dim=0)
-                    flatten_batch_features = []
-                    for chunk in chunks:
-                        chunk = chunk.unsqueeze(0)
-                        with t.no_grad():
-                            chunk_features = self.model.features(chunk.to(self.device))
-                            chunk_features = self.model.new_fc(chunk_features)
-                            flatten_batch_features.append(chunk_features.squeeze(0))
-                    flatten_batch_features = t.cat(flatten_batch_features, dim=0)
-                    batch_features = flatten_batch_features.view((batch_size, 
-                                                                  n_frames, 
-                                                                  *flatten_batch_features.shape[1:]))
+                n_chunks = int(np.ceil(len(flattened_batch_input)/128))
+                chunks = t.chunk(flattened_batch_input, n_chunks, dim=0)
+                flatten_batch_features = []
+                for chunk in chunks:
+                    chunk = chunk.unsqueeze(0)
+                    with t.no_grad():
+                        chunk_features = self.model.features(chunk.to(self.device))
+                        chunk_features = self.model.new_fc(chunk_features)
+                        flatten_batch_features.append(chunk_features.squeeze(0))
+                flatten_batch_features = t.cat(flatten_batch_features, dim=0)
+                batch_features = flatten_batch_features.view((batch_size, 
+                                                              n_frames, 
+                                                              *flatten_batch_features.shape[1:]))
 
-                    total_instances += batch_size
-                    self._append(batch_features, batch_labels, batch_size, feature_writer)
+                total_instances += batch_size
+                self._append(batch_features, batch_labels, batch_size, feature_writer)
         return total_instances
 
     def _append(self, batch_features, batch_labels, batch_size, feature_writer):
@@ -73,5 +72,5 @@ class FeatureExtractor:
         assert len([batch_labels]) == batch_size
         assert len(batch_features) == batch_size
         batch_features = batch_features.squeeze(0).cpu().numpy()
-#         print(batch_narration_id, "DONE")
+
         feature_writer.append(batch_narration_id, batch_features, batch_labels)
